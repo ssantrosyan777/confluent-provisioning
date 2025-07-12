a867414558b4ee476e3b18ef2f5188f7b808719c
Sevak Santrosyan <sevak.santrosyan@bridgewise.com>
2025-07-12 13:40:41 +0400

    Update backend.tf

--- provisioning/backend.tf
+++ provisioning/backend.tf
@@ -1,1 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.83.0"
+    }
+    confluent = {
+      source  = "confluentinc/confluent"
+      version = ">= 2.25.0"
+    }
+  }
+  backend "s3" {
+    bucket = ""
 
+  }
+  required_version = ">= 1.5"
+}
