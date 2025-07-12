ac78279844da58a918f9ac435796e1121e50f5a4
Sevak Santrosyan <sevak.santrosyan@bridgewise.com>
2025-07-12 13:45:40 +0400

    Update data_lookups.tf

--- provisioning/data_lookups.tf
+++ provisioning/data_lookups.tf
@@ -8,7 +8,7 @@ data "confluent_environment" "env" {
 
 data "confluent_service_account" "tf_sa" {
   count = var.env != "" ? 1 : 0
-  id    = "sa-ojvgw9"
+  id    = "confluent_service_account ID"
 }
 
 data "confluent_schema_registry_cluster" "schema_registry" {
