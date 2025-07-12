ad1a40e949c518d023856f8bc4551136aacbfa98
Sevak Santrosyan <sevak.santrosyan@bridgewise.com>
2025-07-12 13:48:19 +0400

    Create resources_confluent_acl.tf

---
+++ provisioning/resources_confluent_acl.tf
@@ -0,0 +1,20 @@
+resource "confluent_kafka_acl" "topic1_acl" {
+  resource_type = "TOPIC"
+  resource_name = "name of the topic1"
+  pattern_type  = "LITERAL"
+  principal     = "User:${confluent_service_account.topic1_sa.id}"
+  host          = "*"
+  operation     = "ALL"
+  permission    = "ALLOW"
+  rest_endpoint = confluent_kafka_cluster.advisor_cluster.0.rest_endpoint
+  credentials {
+    key    = confluent_api_key.insights_processor_api_key.0.id
+    secret = confluent_api_key.insights_processor_api_key.0.secret
+  }
+  kafka_cluster {
+    id = confluent_kafka_cluster.advisor_cluster.0.id
+  }
+  lifecycle {
+    prevent_destroy = true
+  }
+}
