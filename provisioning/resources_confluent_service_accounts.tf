325a612fda76cdc4b75a677efa43635c65f18b54
Sevak Santrosyan <sevak.santrosyan@bridgewise.com>
2025-07-12 14:34:29 +0400

    Update resources_confluent_service_accounts.tf

--- provisioning/resources_confluent_service_accounts.tf
+++ provisioning/resources_confluent_service_accounts.tf
@@ -1,4 +1,4 @@
 resource "confluent_service_account" "topic1_sa" {
   display_name = local.topic1_confluent_sa_name
-  description  = "Service Account for access EKS Topic1 "
+  description  = "Service Account for access Topic1 Service"
 }
